fbe5fdfcd30312e196e85d5c3f2164716f7db00a
EC2 Default User <ec2-user@ip-172-20-5-254.us-west-2.compute.internal>
2017-08-09 21:50:49 +0000

    Adding script to set env variables.

---
+++ etc/setenv.sh
@@ -0,0 +1,5 @@
+# script to set a specici environment
+export OODT_CONFIG=/shared_data/test/src/oodt-0.3-rmq-test-workflow
+export OODT_ARCHIVE=/shared_data/test/archive
+export OODT_JOBS=/shared_data/test/pges/test-workflow/jobs
+
