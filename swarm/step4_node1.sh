6724db61d129992ad46253a789d29bcf2bda0300
EC2 Default User <ec2-user@ip-172-20-5-254.us-west-2.compute.internal>
2017-08-09 21:33:34 +0000

    Adding utility scripts.

--- swarm/step4_node1.sh
+++ swarm/step4_node1.sh
@@ -7,5 +7,5 @@ cids=`docker ps | grep oodt-rabbitmq | awk '{print $1}' | awk '{print $1}'`
 cid=`echo $cids | awk '{print $1;}'`
 echo $cid
 
-NJOBS=10
+NJOBS=10000
 docker exec -it $cid sh -c "cd /usr/local/oodt/rabbitmq; python test_workflow_driver.py $NJOBS"
