23afd13984ab87848a3fd6d5ddc64548fecb21f2
EC2 Default User <ec2-user@ip-172-20-5-254.us-west-2.compute.internal>
2017-08-13 08:02:01 +0000

    Writing out log file.

--- rabbitmq_clients/test_workflow_driver.py
+++ rabbitmq_clients/test_workflow_driver.py
@@ -1,19 +1,39 @@
 # Python script to submit N runs of the 'test-workflow'.
 # After submitting all runs, the script will wait until the message queue is empty.
 #
-# Usage: python ecostress_driver.py <number_of_runs>
+# Usage: python test_workflow_driver.py <number_of_runs>
 
 import logging
 import sys
 import datetime
 import time
 from rabbitmq_producer import publish_messages, wait_for_queues
+import threading
 
 LOG_FORMAT = '%(levelname)s: %(message)s'
 LOGGER = logging.getLogger(__name__)
-LOG_FILE = "rabbitmq_producer.log" # in current directory
+LOG_FILE = "test_workflow_driver.log" # in current directory
 TEST_WORKFLOW = 'test-workflow'
-SLEEP_TIME = 0.5 # time to wait before sending the next message
+SLEEP_TIME = 0 # time to wait before sending the next message
+
+def worker(number_of_runs):
+    '''Function that sends the messages to execute the runes.'''
+
+    # loop over runs=workflows
+    for irun in range(1, number_of_runs+1):
+        LOGGER.info("Submitting messages for run #: %s" % irun)
+   
+        msg_queue = TEST_WORKFLOW
+        num_msgs = 1
+        msg_dict = { 'Dataset':'abc', 'Project':'123', 'Run':irun }
+   
+        publish_messages(msg_queue, num_msgs, msg_dict)
+   
+        # wait before submitting the next run
+        time.sleep(SLEEP_TIME)
+
+    return
+
 
 def main(number_of_runs):
     
@@ -22,22 +42,13 @@ def main(number_of_runs):
     startTime = datetime.datetime.now()
     logging.critical("Start Time: %s" % startTime.strftime("%Y-%m-%d %H:%M:%S") )
 
-    
-    # loop over runs=workflows
-    for irun in range(1, number_of_runs+1):
-        LOGGER.info("Submitting messages for run #: %s" % irun)
-        
-        msg_queue = TEST_WORKFLOW
-        num_msgs = 1
-        msg_dict = { 'Dataset':'abc', 'Project':'123', 'Run':irun }
-        
-        publish_messages(msg_queue, num_msgs, msg_dict)
-            
-        # wait before submitting the next run
-        #time.sleep(SLEEP_TIME)
+    # send all messages in a separare thread
+    # do not wait till completion
+    t = threading.Thread(target=worker, args=(number_of_runs,))
+    t.start()
     
     # wait for RabbitMQ server to process all messages in all queues
-    wait_for_queues(delay_secs=10)
+    wait_for_queues(delay_secs=10, sleep_secs=10)
     
     stopTime = datetime.datetime.now()
     logging.critical("Stop Time: %s" % stopTime.strftime("%Y-%m-%d %H:%M:%S") )
